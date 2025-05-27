5f6d6fb75bad67db369854b50568ce0151834491
ToryThompson1015 <tthompson@perscholas.org>
2025-05-26 20:32:53 -0400

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/BrickByBrick' : '',
 };
 
 module.exports = nextConfig; 
