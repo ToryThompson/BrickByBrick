67f2189513c6683a2229b408c9ece6c763ccbd08
ToryThompson1015 <tthompson@perscholas.org>
2025-05-27 18:40:42 -0400

    Remove basePath from next.config.js for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     unoptimized: true,
     domains: ['images.unsplash.com'],
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/BrickByBrick' : '',
+  // basePath: process.env.NODE_ENV === 'production' ? '/BrickByBrick' : '',
   env: {
     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
